a2907885b126a08b98d5dd72a6f2debe3b17e4ed
Aman <89342512+amnxd@users.noreply.github.com>
2024-10-13 02:30:01 +0530

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-const socket = io('https://chat-96mu.onrender.com')   //ws://localhost:3500
+const socket = io('https://chat.thebenevolent.in/')   //ws://localhost:3500
 
 const msgInput = document.querySelector('#message')
 const nameInput = document.querySelector('#name')
